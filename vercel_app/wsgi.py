bfeae326f0a3bc1aefa2c4007f9df5004d906104
edihasrin <edyhasryn7@gmail.com>
2023-03-11 01:35:18 +0800

    tes

--- vercel_app/wsgi.py
+++ vercel_app/wsgi.py
@@ -13,4 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
 
-app = get_wsgi_application()
+# untuk lokal
+# application = get_wsgi_application()
+
+# Untuk deploy
+app = get_wsgi_application()         
