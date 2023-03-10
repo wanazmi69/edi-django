6af11b29635148268cb3db1999bc0c624013ce72
edihasrin <edyhasryn7@gmail.com>
2023-03-11 01:38:54 +0800

    coba begini

--- vercel_app/wsgi.py
+++ vercel_app/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
 
 # untuk lokal
-# application = get_wsgi_application()
+application = get_wsgi_application()
 
 # Untuk deploy
 app = get_wsgi_application()         
